1d182ccaa6ea5086584f1b82967eb581573eca39
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-23 14:40:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
   array.each_with_index do |element, index|
-    puts "#{index}+1 #{element}"
+    puts "#{index + 1} #{element}"
   end
 end
 
