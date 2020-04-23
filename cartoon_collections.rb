209dfe295f01a0de37efe7f73967dbc68601f1b9
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-23 14:34:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(array)# code an argument here
   # Your code here
   array.each_with_index do |elemenet, index|
     puts "#{index} #{element}"
-  end  
+  end
 end
 
 def summon_captain_planet# code an argument here
