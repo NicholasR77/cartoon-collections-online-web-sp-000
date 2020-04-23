372e84406fb985696c063daec10e3aed7d6d2620
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-23 14:50:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,16 @@
-def roll_call_dwarves(array)# code an argument here
+def roll_call_dwarves(array) # code an argument here
   # Your code here
   array.each_with_index do |element, index|
     puts "#{index + 1} #{element}"
   end
 end
 
-def summon_captain_planet(array)# code an argument here
+def summon_captain_planet(array) # code an argument here
   # Your code here
   array.collect {|element| element.capitalize + "!"}
 end
 
-def long_planeteer_calls(array)# code an argument here
+def long_planeteer_calls(array) # code an argument here
   # Your code here
   array.any? { |element| element.length > 4 }
 end
