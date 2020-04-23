6883244dfb1d3a1357812e5d3e7d516f74b526cb
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-23 14:39:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  array.each_with_index do |elemenet, index|
+  array.each_with_index do |element, index|
     puts "#{index} #{element}"
   end
 end
@@ -14,7 +14,7 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  
+
 end
 
 def find_the_cheese# code an argument here
