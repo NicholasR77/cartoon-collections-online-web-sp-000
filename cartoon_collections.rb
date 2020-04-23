354fc79ff824620250a31cea2e6f595c23a99933
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-23 15:03:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,10 @@ end
 
 def find_the_cheese(array) # code an argument here
   # the array below is here to help
+  i = 0
   cheese_types = ["cheddar", "gouda", "camembert"]
+  while i < cheese_types.length
+    return cheese_types[i] if array.include?(cheese_types[i])
+    i += 1
+  end
 end
