7a979bdd8d8d2f8de842aa15104567b3cd0d3df2
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-23 14:56:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,5 +18,5 @@ end
 def find_the_cheese(array) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find {|element| element = cheese_types}
+  array.find {|element| element = cheese_types[element]}
 end
