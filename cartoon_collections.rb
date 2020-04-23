5284cdf275a8112a1014f45bd71e221aaeb6965b
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-23 14:39:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,6 @@ end
 def summon_captain_planet(array)# code an argument here
   # Your code here
   array.collect {|element| element.capitalize + !}
-  end
 end
 
 def long_planeteer_calls(array)# code an argument here
